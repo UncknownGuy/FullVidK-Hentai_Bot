6b3297965308e45e088666b4533b907640292448
UncknownGuy <cocmaster748596@gmail.com>
2023-09-22 10:34:20 +0530

    Add files via upload

---
+++ settings (1).js
@@ -0,0 +1,14 @@
+//....Replace With Your Bot Token Obtain On Telegram Bot Father...//
+global.TOKEN = '6126806396:AAFe7tpF1wLXZ49E7G3sjFOts3C2NshtuxQ';
+
+//....Replace With Your Group Id One Or Use ( /Id ) Commond After Adding Bot To Obtain Id...//
+global.GROUP_ONE_ID = '';
+
+//....Replace With Your Second Group Id...//
+global.GROUP_TWO_ID = '';
+
+//....Replace With Your Number...//
+global.OWNER = '';
+
+//...Replace With Your Telegram User Name
+global.OWNER_TELE = 'Jleaks69';
